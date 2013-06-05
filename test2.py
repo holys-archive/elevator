75a3bc865e2755bc1ea2b425cf65d1dcc5044076
holys <chendahui007@gmail.com>
2013-06-05 08:21:56 +0800

    rename

--- test2.py
+++ test2.py
@@ -10,8 +10,10 @@ from strs2weibo import  send_weibo
 
 def main():
     topic = u'电影传送门'
+    #query = """SELECT douban_title, download_url, douban_url, lpic_url, douban_id FROM
+    #        movie WHERE douban_id=3319755 and sended=0"""
     query = """SELECT douban_title, download_url, douban_url, lpic_url, douban_id FROM
-            movie WHERE douban_id=3319755 and sended=1"""
+            movie WHERE douban_title='倾国之恋' and sended=0"""
     update = "UPDATE movie SET sended=1 WHERE douban_id=?"
     send_weibo(topic, query, update) 
 
